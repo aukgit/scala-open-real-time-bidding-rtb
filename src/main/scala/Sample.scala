dff5f95058c1412c3108e001d9279b372e5ace2e
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-03-27 23:45:38 +0600

    setting up log4j

--- src/main/scala/Sample.scala
+++ src/main/scala/Sample.scala
@@ -1,4 +1,10 @@
+import org.apache.logging.log4j.scala.Logger
+
 object Sample {
+
+
+  private val LOGGER = Logger.getLogger(classOf[Nothing])
+
   def main(args : Array[String]) : Unit = {
     println("Hello World")
   }
