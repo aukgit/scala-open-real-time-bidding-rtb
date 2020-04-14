16927c8376635f3ec858c9236cb9adcbf5bd1877
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-14 13:24:16 +0600

    Updated

--- src/main/scala/com/ortb/persistent/repositoryPattern/traits/RepositoryOperationsAsync.scala
+++ src/main/scala/com/ortb/persistent/repositoryPattern/traits/RepositoryOperationsAsync.scala
@@ -6,7 +6,8 @@ import slick.jdbc.SQLiteProfile.api._
 import com.ortb.implicits.ImplicitsDefinitions.anyRefCaller
 import com.ortb.model.results.RepositoryOperationResult
 import com.ortb.persistent.repositoryPattern.Repository
-import slick.dbio.{Effect, NoStream}
+import io.AppLogger
+import slick.dbio.{NoStream, Effect}
 import slick.sql.FixedSqlAction
 
 import scala.concurrent.Future
@@ -19,22 +20,37 @@ trait RepositoryOperationsAsync[TTable <: AbstractTable[_], TRow <: Null, TKey]
 
   def deleteAsync(entityKey: TKey, entity: TRow):
   Future[RepositoryOperationResult[TRow]] = {
-    this.saveAsync(
-      entityKey = entityKey,
-      entity = entity,
-      dbAction = getQueryByIdSingle(entityKey)
-        .call("delete")
-        .asInstanceOf[FixedSqlAction[Int, NoStream, Effect.Write]],
-      isPerformActionOnExist = true,
-      DatabaseActionType.Delete)
+    try {
+      return this.saveAsync(
+        entityKey = entityKey,
+        entity = entity,
+        dbAction = getQueryByIdSingle(entityKey)
+          .call("delete")
+          .asInstanceOf[FixedSqlAction[Int, NoStream, Effect.Write]],
+        isPerformActionOnExist = true,
+        DatabaseActionType.Delete)
+    } catch {
+      case e: Exception => AppLogger.error(e,
+        s"Delete failed on [id:$entityKey, entity: $entity]")
+    }
+
+    getEmptyResponse
   }
 
   def updateAsync(entityId: TKey, entity: TRow): Future[RepositoryOperationResult[TRow]] = {
-    this.saveAsync(
-      entity = entity,
-      dbAction = getQueryByIdSingle(entityId).update(entity),
-      DatabaseActionType.Update
-    )
+    try {
+      return this.saveAsync(
+        entity = entity,
+        dbAction = getQueryByIdSingle(entityId).update(entity),
+        DatabaseActionType.Update
+      )
+    }
+    catch {
+      case e: Exception => AppLogger.error(e,
+        s"Update failed on [id:$entityId, entity: $entity]")
+    }
+
+    getEmptyResponse
   }
 
   def addOrUpdateAsync(entityId: TKey, entity: TRow): Future[RepositoryOperationResult[TRow]] = {
