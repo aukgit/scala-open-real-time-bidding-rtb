c597be6119527884410cf7454f565a0db678db3b
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-15 19:45:47 +0600

    ContentContextRepository added

---
+++ src/main/scala/com/ortb/persistent/repositories/ContentContextRepository.scala
@@ -0,0 +1,46 @@
+package com.ortb.persistent.repositories
+
+import slick.jdbc.SQLiteProfile.api._
+import com.ortb.manager.AppManager
+import com.ortb.persistent.repositoryPattern.Repository
+import com.ortb.persistent.schema.Tables
+import com.ortb.persistent.schema.Tables._
+import slick.dbio.Effect
+import slick.sql.FixedSqlAction
+
+class ContentContextRepository(appManager : AppManager)
+  extends
+    Repository[Contentcontext, ContentcontextRow, Int](appManager) {
+
+  override def tableName : String = this.contentContextTableName
+
+  override def getEntityId(entity : Option[Tables.ContentcontextRow]) : Int =
+    if (entity.isDefined) entity.get.contentcontextid
+    else -1
+
+  override def setEntityId(
+    entityId : Option[Int],
+    entity   : Option[Tables.ContentcontextRow]) : Option[Tables.ContentcontextRow] = {
+    if (isEmptyGivenEntity(entityId, entity)) {
+      return None
+    }
+
+    Some(entity.get.copy(contentcontextid = entityId.get))
+  }
+
+  override def getAddAction(
+    entity : Tables.ContentcontextRow) =
+    table returning table.map(_.contentcontextid) into
+      ((entityProjection, entityId) => entityProjection.copy(contentcontextid = entityId)) += entity
+
+  override def table = this.contentContexts
+
+  override def getDeleteAction(
+    entityId : Int) : FixedSqlAction[Int, NoStream, Effect.Write] =
+    getQueryById(entityId).delete
+
+  override def getQueryById(
+    id : Int) = table.filter(c => c.contentcontextid === id)
+
+  override def getAllQuery = for {record <- table} yield record
+}
