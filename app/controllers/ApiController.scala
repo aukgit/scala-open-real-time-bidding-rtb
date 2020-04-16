bf4f0d6242e2847298e420503fd6c971c1a763b8
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-16 17:39:33 +0600

    Fixing issues or removing files
    Fixing issues or removing files

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,17 +1,21 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{
+  AbstractController,
+  Action,
+  AnyContent,
+  ControllerComponents
+}
 import play.api.libs.json.Json
 
-class ApiController @Inject()(
-    components: ControllerComponents
-) extends AbstractController(components) {
-  def index = Action { implicit request =>
+class ApiController @Inject()(components: ControllerComponents)
+    extends AbstractController(components) {
+  def index: Action[AnyContent] = Action { implicit request =>
     Ok(Json.obj("hello" -> "world"))
   }
 
-  def ping = Action { implicit request =>
+  def ping: Action[AnyContent] = Action { implicit request =>
     Ok(Json.obj("ping" -> true))
   }
 }
