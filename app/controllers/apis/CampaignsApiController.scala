f46d9ad1f6859a6b427d010ef4f80f7ca7fc2f82
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-18 01:14:03 +0600

    changes in api and web services
    changes in api and web services

--- app/controllers/apis/CampaignsApiController.scala
+++ app/controllers/apis/CampaignsApiController.scala
@@ -3,21 +3,77 @@ package controllers.apis
 import io.circe.generic.auto._
 import io.circe.syntax._
 import javax.inject.Inject
-import play.api.mvc._
+import play.api.mvc.{Action, _}
 import services.CampaignService
-import shared.com.ortb.manager.AppManager
-import shared.com.ortb.persistent.Repositories
-import shared.io.logger.AppLogger
+import shared.com.ortb.persistent.schema.Tables
+import shared.com.ortb.persistent.schema.Tables._
+
+trait WebApi[TTable, TRow, TKey] {
+  def getAll : Action[AnyContent]
+
+  def byId(id : TKey) : Action[AnyContent]
+
+  def add(entity : TRow) : Action[AnyContent]
+
+  def update(entity : TRow) : Action[AnyContent]
+
+  def delete(id : TKey) : Action[AnyContent]
+
+  def addEntities(entities : Iterable[TRow]) : Action[AnyContent]
+
+  def addEntitiesBySinge(entity : TRow, addTimes : Int) : Action[AnyContent]
+}
 
 class CampaignsApiController @Inject()(
-  appManager: AppManager,
   campaignService : CampaignService,
-  components : ControllerComponents)
-  extends AbstractController(components) {
-  def campaigns : Action[AnyContent] = Action { implicit request =>
+  components      : ControllerComponents)
+  extends AbstractController(components) with
+    WebApi[Campaign, CampaignRow, Int] {
+
+  override def getAll : Action[AnyContent] = Action { implicit request =>
     val campaigns = campaignService.getAll
     val json = campaigns.asJson.spaces2
-    // println(json)
+    Ok(json)
+  }
+
+  override def byId(id : Int) : Action[AnyContent] = Action { implicit request =>
+    val campaign = campaignService.getById(id)
+    val json = campaign.asJson.spaces2
+    Ok(json)
+  }
+
+  override def add(
+    entity : Tables.CampaignRow) : Action[AnyContent] = Action { implicit request =>
+    val response = campaignService.add(entity)
+    val json = response.asJson.spaces2
+    Ok(json)
+  }
+
+  override def update(
+    entity : Tables.CampaignRow) : Action[AnyContent] = Action { implicit request =>
+    val response = campaignService.update(entity.campaignid, entity)
+    val json = response.asJson.spaces2
+    Ok(json)
+  }
+
+  override def delete(id : Int) : Action[AnyContent] = Action { implicit request =>
+    val response = campaignService.delete(id)
+    val json = response.asJson.spaces2
+    Ok(json)
+  }
+
+  override def addEntities(
+    entities : Iterable[Tables.CampaignRow]) : Action[AnyContent] = Action { implicit request =>
+    val response = campaignService.addEntities(entities)
+    val json = response.asJson.spaces2
+    Ok(json)
+  }
+
+  override def addEntitiesBySinge(
+    entity : Tables.CampaignRow,
+    addTimes : Int) : Action[AnyContent] = Action { implicit request =>
+    val response = campaignService.addEntities(entity, addTimes)
+    val json = response.asJson.spaces2
     Ok(json)
   }
 }
--- app/services/CampaignService.scala
+++ app/services/CampaignService.scala
@@ -1,16 +1,14 @@
 package services
 
 import com.google.inject.Inject
-import services.traits.BasicPersistentServiceOperations
-import shared.com.ortb.manager.AppManager
+import services.traits.ServiceBasicPersistentOperations
 import shared.com.ortb.persistent.repositories.CampaignRepository
 import shared.com.ortb.persistent.repositories.pattern.RepositoryBase
 import shared.com.ortb.persistent.schema.Tables._
 
 class CampaignService @Inject()(
-  appManager: AppManager,
-  campaignRepository : CampaignRepository)
-  extends BasicPersistentServiceOperations[Campaign, CampaignRow, Int] {
+  campaignRepository                               : CampaignRepository)
+  extends ServiceBasicPersistentOperations[Campaign, CampaignRow, Int] {
 
   val repository : RepositoryBase[Campaign, CampaignRow, Int] =
     campaignRepository
