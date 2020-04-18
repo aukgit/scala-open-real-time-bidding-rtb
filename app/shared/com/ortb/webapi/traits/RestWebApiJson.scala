932d0735608a198c7219c2e6c0a117afac03528b
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-19 01:36:47 +0600

    RestWebApiJson fixing
    RestWebApiJson fixing

--- app/shared/com/ortb/webapi/traits/RestWebApiJson.scala
+++ app/shared/com/ortb/webapi/traits/RestWebApiJson.scala
@@ -1,12 +1,15 @@
 package shared.com.ortb.webapi.traits
 
+import io.circe.{Decoder, Encoder}
+
 trait RestWebApiJson[TTable, TRow, TKey] {
 
-  def toJsonFrom(entity : Iterable[TRow]) : String
+  def fromJsonToEntities(entitiesJsonString : Option[String])(implicit decoder: Decoder[Iterable[TRow]]) : Option[]
 
-  def toEntityJson(entity : TRow) : String
+  def fromEntityToJson(entity : TRow)(implicit decoder: Decoder[TRow]) : String
 
-  def toJson[T](item : T) : String
+  def fromEntityToJson[T](item : T)(implicit encoder: Encoder[T]) : Option[String]
 
-  def toJson[T](items : Iterable[T]) : String
+  def toJson[T](items : Iterable[T])
+    (implicit encoder: Encoder[Iterable[T]]) : Option[String]
 }
