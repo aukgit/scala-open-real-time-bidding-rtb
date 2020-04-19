85c7c8278613936b4920b1b4952276226204ecc9
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-19 20:21:58 +0600

    RepositoryOperationResult refactored
    RepositoryOperationResult refactored

--- app/shared/com/ortb/model/results/RepositoryOperationResult.scala
+++ app/shared/com/ortb/model/results/RepositoryOperationResult.scala
@@ -1,8 +1,10 @@
 package shared.com.ortb.model.results
 
 import shared.com.ortb.enumeration.DatabaseActionType.DatabaseActionType
-import shared.com.ortb.model.wrappers.persistent.EntityWrapper
-import slick.dbio.DatabaseAction
 
 case class RepositoryOperationResult[TRow, TKey](
-  isSuccess : Boolean, entityId : Option[TKey], entity : Option[TRow], actionType : DatabaseActionType, message : String = null)
+  isSuccess : Boolean,
+  entityId : Option[TKey],
+  entity : Option[TRow],
+  actionType : DatabaseActionType,
+  message    : String = null)
