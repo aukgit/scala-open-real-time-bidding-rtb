f2a2ed3f18eea632e638c39cd2383dfa0be0f674
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-20 07:59:32 +0600

    Repository -> Refactored and Restructured folder forRepositoryOperationsAsyncImplementation
    # Repository -> Refactored and Restructured folder forRepositoryOperationsAsyncImplementation
    - [x] Refactored and Restructured folder for Repository Async Operations
    - [x] RepositoryAddOperationsAsyncImplementation
    - [x] RepositoryAddOrUpdateOperationsAsyncImplementation
    - [x] RepositoryDeleteOperationsAsyncImplementation
    - [x] RepositoryUpdateOperationsAsyncImplementation
    - [x] Refactor Implementions of RepositoryOperationsAsyncImplementation - Complete

---
+++ app/shared/com/repository/traits/implementions/operations/mutations/async/RepositoryAddOrUpdateOperationsAsyncImplementation.scala
@@ -0,0 +1,25 @@
+package shared.com.repository.traits.implementions.operations.mutations.async
+
+import shared.com.ortb.model.repository.response.RepositoryOperationResultModel
+import shared.com.repository.RepositoryBase
+import shared.com.repository.traits.operations.mutations.async.RepositoryAddOrUpdateOperationsAsync
+
+import scala.concurrent.Future
+
+trait RepositoryAddOrUpdateOperationsAsyncImplementation[TTable, TRow, TKey]
+  extends RepositoryAddOrUpdateOperationsAsync[TTable, TRow, TKey] {
+  this : RepositoryBase[TTable, TRow, TKey] =>
+
+  def addOrUpdateAsync(
+    entityId : TKey,
+    entity : TRow
+  ) : Future[RepositoryOperationResultModel[TRow, TKey]] = {
+    if (isExists(entityId)) {
+      // update
+      return updateAsync(entityId, entity)
+    }
+
+    // add
+    addAsync(entity)
+  }
+}
--- app/shared/com/repository/traits/implementions/operations/mutations/async/RepositoryOperationsAsyncImplementation.scala
+++ app/shared/com/repository/traits/implementions/operations/mutations/async/RepositoryOperationsAsyncImplementation.scala
@@ -1,114 +1,13 @@
 package shared.com.repository.traits.implementions.operations.mutations.async
 
-import shared.com.ortb.enumeration.DatabaseActionType
-import shared.com.ortb.model.repository.response.RepositoryOperationResultModel
 import shared.com.repository.RepositoryBase
-import shared.com.repository.traits.operations.mutations.async.RepositoryOperationsAsync
-import shared.io.loggers.AppLogger
-import slick.dbio.{Effect, NoStream}
-import slick.sql.FixedSqlAction
-
-import scala.concurrent.Future
+import shared.com.repository.traits.operations.mutations.async._
 
 trait RepositoryOperationsAsyncImplementation[TTable, TRow, TKey]
-  extends RepositoryOperationsAsync[TTable, TRow, TKey] {
+  extends RepositoryOperationsAsync[TTable, TRow, TKey]
+    with RepositoryAddOperationsAsyncImplementation[TTable, TRow, TKey]
+    with RepositoryUpdateOperationsAsyncImplementation[TTable, TRow, TKey]
+    with RepositoryDeleteOperationsAsyncImplementation[TTable, TRow, TKey]
+    with RepositoryAddOrUpdateOperationsAsyncImplementation[TTable, TRow, TKey] {
   this : RepositoryBase[TTable, TRow, TKey] =>
-  def getDeleteAction(
-    entityId : TKey
-  ) : FixedSqlAction[Int, NoStream, Effect.Write]
-
-  def deleteAsync(
-    entityId : TKey
-  ) : Future[RepositoryOperationResultModel[TRow, TKey]] = {
-    val entity = getById(entityId)
-    val actionType = DatabaseActionType.Delete
-
-    try {
-      val deleteAction = getDeleteAction(entityId)
-
-      return this.saveAsync(
-        entity = entity,
-        dbAction = deleteAction,
-        actionType
-      )
-    } catch {
-      case e : Exception =>
-        AppLogger.error(
-          e,
-          s"${headerMessage} Delete failed on [id:$entityId, entity: $entity]"
-        )
-    }
-
-    getEmptyResponseForInFuture(actionType)
-  }
-
-  def addOrUpdateAsync(
-    entityId : TKey,
-    entity : TRow
-  ) : Future[RepositoryOperationResultModel[TRow, TKey]] = {
-    if (isExists(entityId)) {
-      // update
-      return updateAsync(entityId, entity)
-    }
-
-    // add
-    addAsync(entity)
-  }
-
-  def addAsync(entity : TRow) : Future[RepositoryOperationResultModel[TRow, TKey]] = {
-    val actionType = DatabaseActionType.Create
-
-    try {
-      val action = getAddAction(entity)
-
-      return this.saveAsync(dbAction = action, DatabaseActionType.Create)
-    } catch {
-      case e : Exception =>
-        AppLogger.error(e, s"${headerMessage} Add failed on [entity: $entity]")
-    }
-
-    getEmptyResponseForInFuture(actionType)
-  }
-
-  /**
-   * if entityId is not matching with given entity id then recreates new entity and set the id given and then perform
-   * the action.
-   *
-   * @param entityId
-   * @param entity
-   *
-   * @return
-   */
-  def updateAsync(
-    entityId : TKey,
-    entity : TRow
-  ) : Future[RepositoryOperationResultModel[TRow, TKey]] = {
-    val actionType = DatabaseActionType.Update
-
-    try {
-      if (entityId != getEntityId(Some(entity))) {
-        val entity2 = setEntityId(Some(entityId), Some(entity))
-
-        return this.saveAsync(
-          entity = entity2,
-          dbAction = getQueryById(entityId).update(entity2.get),
-          DatabaseActionType.Update
-        )
-      }
-
-      return this.saveAsync(
-        entity = Some(entity),
-        dbAction = getQueryById(entityId).update(entity),
-        DatabaseActionType.Update
-      )
-    } catch {
-      case e : Exception =>
-        AppLogger.error(
-          e,
-          s"${headerMessage} Update failed on [id:$entityId, entity: $entity]"
-        )
-    }
-
-    getEmptyResponseForInFuture(actionType)
-  }
 }
