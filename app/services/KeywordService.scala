6a626249a4dba1cb0aea62c3325e3b5c4e9356cc
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-25 01:55:19 +0600

    Code clean up on the services
    Code clean up on the services

--- app/services/KeywordService.scala
+++ app/services/KeywordService.scala
@@ -4,7 +4,7 @@ import com.google.inject.Inject
 import services.core.AbstractBasicPersistentService
 import shared.com.ortb.persistent.repositories._
 import shared.com.ortb.persistent.schema.Tables._
-import shared.com.repository.RepositoryBase
+
 class KeywordService @Inject()(
   injectedRepository : KeywordRepository)
   extends AbstractBasicPersistentService[Keyword, KeywordRow, Int] {
