c50527a51e3f335f213adfac05d829d4cf1a1f19
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-04-26 17:22:35 +0600

    changes in DSP logic
    changes in DSP logic

--- app/controllers/rtb/DspBidderSample1.scala
+++ app/controllers/rtb/DspBidderSample1.scala
@@ -3,13 +3,29 @@ package controllers.rtb
 import shared.com.ortb.enumeration.DemandSidePlatformBiddingAlgorithmType.DemandSidePlatformBiddingAlgorithmType
 import shared.com.ortb.importedModels.biddingRequests.Impression
 import shared.com.ortb.model.results.DspBidderRequestModel
-
+import shared.com.ortb.persistent.schema.Tables._
+import shared.com.ortb.persistent.schema.Tables
+import shared.com.ortb.persistent.schema.Tables._
+import shared.com.repository.RepositoryBase
+import slick.jdbc.SQLiteProfile.api._
 import scala.collection.mutable.ArrayBuffer
+import slick.dbio.Effect
+import slick.jdbc.SQLiteProfile
+import slick.lifted.Query
+import slick.sql.FixedSqlAction
+
+case class BidFailedReasonsModel(
+  bidRequests: Seq[BidrequestRow],
+  bidResponses: Seq[BidresponseRow]
+)
 
 class DspBidderSample1(algorithmType : DemandSidePlatformBiddingAlgorithmType)
   extends DspBidder {
   val defaultIncrementNumber = 0.01
   val defaultStaticDeal = 0.03
+
+  override def getBidPrices(
+    request : DspBidderRequestModel) : Option[DspBidderResultModel] = ???
 
   override def getBidStatic(
     request : DspBidderRequestModel) : Option[DspBidderResultModel] = {
@@ -69,15 +85,26 @@ class DspBidderSample1(algorithmType : DemandSidePlatformBiddingAlgorithmType)
     Some(dspBidderResultModel)
   }
 
-  def getLastFailedDeals(request : DspBidderRequestModel) = {
+  def getLastFailedDeals(request : DspBidderRequestModel): BidFailedReasonsModel = {
     val controller = request.controller
-    val w = controller.repositories.
+    val repositories = controller.repositories
+    val auctionsTable  = repositories.auctions
+    val bidResponseTable = repositories.bidResponses
+    val bidRequestsTable =repositories.bidRequests
+
+    val joins = for {
+      (bidRequest, _)
+    }
+
+    bidResponseTable.filter(r => r.iswontheauction != true )
+
+    val failedBidRequests = bidRequestsTable.filter(r=> r.iswontheauction != true)
+    val sortedFailedRequests= failedBidRequests.sorted(w=> w.createddate)
+
   }
 
   override def getBidActual(
-    request : DspBidderRequestModel) : Option[DspBidderResultModel] = {
-
-  }
+    request : DspBidderRequestModel) : Option[DspBidderResultModel] = ???
 
   override def getBidActualNoContent(
     request : DspBidderRequestModel) : Option[DspBidderResultModel] = ???
