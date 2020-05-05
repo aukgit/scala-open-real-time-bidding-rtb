3745e9995cac3bb8c4e0d0a28bb61b2d59e03306
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-05-05 16:19:33 +0600

    Database generator updated
    Database generator updated

--- app/shared/com/ortb/executors/DatabaseEngineCodeGenerator.scala
+++ app/shared/com/ortb/executors/DatabaseEngineCodeGenerator.scala
@@ -3,10 +3,36 @@ package shared.com.ortb.executors
 import shared.com.ortb.manager.AppManager
 import shared.com.ortb.manager.traits.DefaultExecutionContextManager
 import slick.codegen.SourceCodeGenerator
+import slick.driver
+import slick.jdbc.JdbcProfile
 import slick.jdbc.meta.MTable
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{ Await, Future }
+import slick.{model => m}
+
+class CustomSourceCodeGenerator(model: m.Model) extends SourceCodeGenerator(model) {
+
+  // add some custom imports
+  // TODO: fix these imports to refer to your JdbcSupport and your Joda imports
+  override def code = "import com.github.tototoshi.slick.PostgresJodaSupport._\n" + "import org.joda.time.DateTime\n" + super.code
+
+  override def Table = new Table(_) {
+    override def Column = new Column(_) {
+
+      // munge rawType -> SQL column type HERE (scaladoc in Slick 2.1.0 is outdated or incorrect, GeneratorHelpers#mapJdbcTypeString does not exist)
+      // you can filter on model.name for the column name or model.tpe for the column type
+      // your IDE won't like the String here but don't worry, the return type the compiler expects here is String
+      override def rawType : String = model.tpe match {
+        case "java.sql.Timestamp"               => "DateTime" // kill j.s.Timestamp
+        case _ => {
+          //          println(s"${model.table.table}#${model.name} tpe=${model.tpe} rawType=${super.rawType}")
+          super.rawType
+        }
+      }
+    }
+  }
+}
 
 object DatabaseEngineCodeGenerator extends App with DefaultExecutionContextManager {
   lazy val appManager = new AppManager
@@ -30,8 +56,9 @@ object DatabaseEngineCodeGenerator extends App with DefaultExecutionContextManag
     Some(mTables))
 
   val model = db.run(dbio)
-  val future : Future[SourceCodeGenerator] = model.map(model => new SourceCodeGenerator(model))
-  val codegen : SourceCodeGenerator = Await.result(future, Duration.Inf)
+  val eventualSourceCodeGenerator : Future[SourceCodeGenerator] = model.map(model => new CustomSourceCodeGenerator(model))
+  val codegen : SourceCodeGenerator = Await.result(eventualSourceCodeGenerator, Duration.Inf)
+
   codegen.writeToFile(
     databaseProfile,
     outputDir,
