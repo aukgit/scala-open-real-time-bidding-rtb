2f70acf78fdfb5e96d9178dade6ad4990cfd0f69
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-05-23 04:57:36 +0600

    - Fixed PrimitiveTypeStringConversion

--- app/shared/io/extensions/traits/primitiveTypes/PrimitiveTypeStringConversion.scala
+++ app/shared/io/extensions/traits/primitiveTypes/PrimitiveTypeStringConversion.scala
@@ -3,11 +3,16 @@ package shared.io.extensions.traits.primitiveTypes
 import shared.io.helpers.EmptyValidateHelper
 
 trait PrimitiveTypeStringConversion {
-  lazy private val isTrue = EmptyValidateHelper.isStringDefined(s) ||
-    s.equalsIgnoreCase("true") ||
-    s == "1" ||
-    s.equalsIgnoreCase("yes")
   val s : String
+
+  lazy private val isDefined = EmptyValidateHelper.isStringDefined(s)
+  lazy private val isTrueString = isDefined && s.equalsIgnoreCase("true")
+  lazy private val isYesString = isDefined && s.equalsIgnoreCase("yes")
+
+  lazy private val isTrue =
+    isTrueString ||
+      s == "1" ||
+      isYesString
 
   def toBoolString : String = toBoolean.toString
 
